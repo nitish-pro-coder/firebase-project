a2109481764fcb9c6ce75dd887882f74786eb588
Nitish <nitish@warblerit.com>
2023-06-12 17:49:12 +0530

    changes
    changes

--- controllers/MailController.js
+++ controllers/MailController.js
@@ -63,6 +63,7 @@ transporter.sendMail(mailOptions, (error, info) => {
       MOdifiedDate: updated_at_timestamp,
       login_otp: randomSixDigitNumber
     })
+    res.status(200).send('Email sent successfully');
    
   })
    
@@ -73,7 +74,7 @@ transporter.sendMail(mailOptions, (error, info) => {
 });
 } catch (error) {
 console.error('Error occurred:', error);
-res.status(200).send('Email sent successfully');
+
 res.status(500).send('Error occurred while sending email');
 }
 }
